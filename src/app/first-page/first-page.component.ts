60b5e081b726f139283f67da9ea580b77c7e9999
clocke3 <clocke3@ycp.edu>
2025-04-22 17:51:05 -0400

    added ahreum png to first page

--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -3,9 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first-page',
   imports: [],
-  templateUrl: './first-page.component.html',
+  template:
+    `<div>
+    <div class="content">
+      <!-- main page -->
+      <div class="mainPage">
+          <img [src]="logoUrl" [alt]="logoAlt">
+      </div>
+    </div>
+  </div>`,
   styleUrl: './first-page.component.css'
 })
 export class FirstPageComponent {
-
+  logoUrl = "assets/images/ahreumLogo.png";
+  logoAlt = "Ahreum logo";
 }
