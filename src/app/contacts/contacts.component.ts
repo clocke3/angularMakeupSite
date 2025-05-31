f6fc6635bae5ed8e35fb56784cad0a85c4e620c4
Chihea Locke <clocke3@ycp.edu>
2025-05-31 15:07:41 -0400

    adding hovers to text in contact component, adding back to top button

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { QuickViewModalComponent } from "../quick-view-modal/quick-view-modal.component";
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-contacts',
-  imports: [QuickViewModalComponent],
+  imports: [],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
 export class ContactsComponent {
+  scrollToTop(){
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 
 }
