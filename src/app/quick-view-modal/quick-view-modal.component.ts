6bc2fdb49f475ec0daf7d05606802617428fad37
Chihea Locke <clocke3@ycp.edu>
2025-05-31 14:27:59 -0400

    getting basis for modal component to be dynamic

--- src/app/quick-view-modal/quick-view-modal.component.ts
+++ src/app/quick-view-modal/quick-view-modal.component.ts
@@ -1,13 +1,29 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Inject,
+  Input,
+  Output,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 
 @Component({
   selector: 'app-quick-view-modal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quick-view-modal.component.html',
-  styleUrl: './quick-view-modal.component.css'
+  styleUrl: './quick-view-modal.component.css',
 })
 export class QuickViewModalComponent {
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+  @ViewChildren('colorBtn') colorButtons!: QueryList<
+    ElementRef<HTMLButtonElement>
+  >;
+
   @Input() imageUrl!: string;
   @Input() productName!: string;
   @Input() description!: string;
@@ -15,8 +31,45 @@ export class QuickViewModalComponent {
   @Input() price!: string;
 
   @Output() closeEvent = new EventEmitter<void>();
+  @Output() newColor = new EventEmitter<string>();
+  selectedColor: string | null = null;
+  savedColorBtn: HTMLButtonElement | null = null;
+  showIcon = false;
 
   close() {
     this.closeEvent.emit();
+  }
+
+  chooseColor(input: string) {
+
+    const elem = this.colorButtons.filter((btn) =>
+      btn.nativeElement.textContent
+        ?.trim()
+        .toLowerCase()
+        .includes(input.toLowerCase())
+    );
+
+    if (!elem || elem.length === 0) {
+      console.warn('No matching button found for:', input);
+      return;
+    }
+
+    if (this.savedColorBtn) {
+      const oldDiv = this.savedColorBtn.querySelector('div');
+      if (oldDiv) {
+        oldDiv.classList.remove('placeIconInBtn');
+      }
+      this.showIcon = false;
+    }
+
+    const newBtn = elem[0].nativeElement;
+    const newDiv = newBtn.querySelector('div');
+    if (newDiv) {
+      newDiv.classList.add('placeIconInBtn');
+    }
+
+    this.selectedColor = input;
+    this.savedColorBtn = newBtn;
+    this.showIcon = true;
   }
 }
