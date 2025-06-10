657124364fb5323d9ccb74386f5c69fde673481a
Chihea Locke <clocke3@ycp.edu>
2025-06-10 19:45:05 -0400

    very simple session storage setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ShoppingCartComponent } from './shared/shopping-cart/shopping-cart.component';
 import { MainEndpointComponent } from './main-endpoint/main-endpoint.component';
+import { AccountComponent } from './auth/accounts/account.component';
 
 export const routes: Routes = [
     { path: '', component: MainEndpointComponent },
+    { path: 'account', component: AccountComponent },
     { path: 'cart', component: ShoppingCartComponent }
 ];
