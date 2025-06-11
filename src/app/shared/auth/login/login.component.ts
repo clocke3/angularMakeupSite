46355aee9ff86d9ce763511041d968a341a19154
Chihea Locke <clocke3@ycp.edu>
2025-06-10 20:59:55 -0400

    light styling of login page

--- src/app/shared/auth/login/login.component.ts
+++ src/app/shared/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-login',
   imports: [],
+  standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
